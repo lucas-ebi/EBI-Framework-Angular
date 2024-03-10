c053c53c00367fee359d7d0ddcbc2972219e7e7c
lucasemdb <lucas@ebi.ac.uk>
2024-03-10 21:53:37 +0000

    Update EBI header image URL

--- src/app/components/ebi-header/ebi-header.component.ts
+++ src/app/components/ebi-header/ebi-header.component.ts
@@ -8,7 +8,7 @@ import { Component, Input } from '@angular/core';
 export class EbiHeaderComponent {
   @Input() title: string;
   @Input() href: string = "/";
-  @Input() image: string = "https://www.ebi.ac.uk/web_guidelines/images/banners/EBI_SERVICES_Banner.jpg";
+  @Input() image: string = "https://www.ebi.ac.uk/web_guidelines/images/banners/EBI_SERVICES_Banner_2016.jpg";
   @Input() color: string = "#091316";
   @Input() links: any = [
     {"title": "New submission", "href": "/submission"},
